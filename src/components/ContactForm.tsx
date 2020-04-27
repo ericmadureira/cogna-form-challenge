a7b4765ca10b6240eb498d861150ae2740c92f79
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-26 22:22:21 -0300

    feat: contact page
    Bootstrap contact page and adds it to the app.

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactForm = () => {
+  return (
+    <form action="">
+      <span>Input</span>
+      <input type="text"/>
+    </form>
+  );
+}
+
+export default ContactForm;
