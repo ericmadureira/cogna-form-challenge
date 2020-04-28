70e96eea8bc3149b5e297486a5ab88495db38bc3
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-28 00:29:28 -0300

    feat: initial form values and types
    Adds contact form initial values and types to the modules.

--- src/constants/formOptions.ts
+++ src/constants/formOptions.ts
@@ -3,15 +3,25 @@ type ContactSubject = {
   label: string;
 }
 
-const contactSubjects: ContactSubject[] = [
+type ContactFormValues = {
+  name: string;
+  email: string;
+  membership: string;
+  help: number;
+  message: string;
+}
+
+export const contactSubjects: ContactSubject[] = [
   { id: 1, label: 'I have question about my membership' },
   { id: 2, label: 'I have technical question' },
   { id: 3, label: 'I would like to change membership' },
   { id: 4, label: 'Other question' },
 ];
 
-const formOptions = {
-  contactSubjects,
+export const initialContactFormValues: ContactFormValues = {
+  name: '',
+  email: '',
+  membership: 'A',
+  help: 1,
+  message: '',
 };
-
-export default formOptions;
